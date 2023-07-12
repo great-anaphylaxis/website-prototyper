812685662d3e4d7f490712eef67014e0c277c1e2
Jhareign <74128037+Jhareign@users.noreply.github.com>
2023-07-12 16:27:41 +0800

    type1-7 with some animations

--- animation.js
+++ animation.js
@@ -1,5 +1,29 @@
+// for advanced animations
+
+function addAnimation(element, className) {
+    element.classList.add(className);
+}
+
+function removeAnimation(element, className) {
+    element.classList.remove(className);
+}
 
 
-function playAnimation() {
-    
+/* type1-7 */
+let type1_7 = document.getElementsByClassName("type1-7");
+
+for (let i = 0; i < type1_7.length; i++) {
+    let items = type1_7[i].getElementsByClassName("item");
+
+    for (let j = 0; j < items.length; j++) {
+        items[j].addEventListener("mouseenter", e => {
+            addAnimation(items[j].getElementsByClassName("progress")[0], "type1-7_animation1-1");
+        });
+
+        items[j].getElementsByClassName("progress")[0].addEventListener("animationend", e => {
+            if (e.animationName === "type1-7_animation1-1") {
+                removeAnimation(e.target, "type1-7_animation1-1");
+            }
+        })
+    }
 }
