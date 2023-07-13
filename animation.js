47d48e32b84f7ea0eda2260ff187c767c177ff96
Jhareign <74128037+Jhareign@users.noreply.github.com>
2023-07-13 11:25:29 +0800

    type1-8

--- animation.js
+++ animation.js
@@ -27,3 +27,33 @@ for (let i = 0; i < type1_7.length; i++) {
         })
     }
 }
+
+
+
+
+/* type1-8 */
+let type1_8 = document.getElementsByClassName("type1-8");
+
+for (let i = 0; i < type1_8.length; i++) {
+    let items = type1_8[i].querySelectorAll(".container1 > .list > .item") || [];
+
+    for (let j = 0; j < items.length; j++) {
+        items[j].addEventListener("mouseenter", e => {
+            let items2 = items[j].querySelectorAll(".container2 > .list > .item");
+
+            for (let k = 0; k < items2.length; k++) {
+                console.log(items2[k]);
+                addAnimation(items2[k], "type1-8_animation1-1");
+            }
+        });
+
+        items[j].addEventListener("mouseleave", e => {
+            let items2 = items[j].querySelectorAll(".container2 > .list > .item");
+            
+            for (let k = 0; k < items2.length; k++) {
+                console.log(items2[k]);
+                removeAnimation(items2[k], "type1-8_animation1-1");
+            }
+        })
+    }
+}
